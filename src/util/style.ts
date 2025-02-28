026877283c67498e050113e7daa7d515a50b4283
someoneonsmile <someoneonsmile@gmail.com>
2025-02-28 16:18:04 +0800

    feat: util/style add isPreferDark fn

--- src/util/style.ts
+++ src/util/style.ts
@@ -7,3 +7,10 @@ import { twMerge } from 'tailwind-merge'
 export const cn = function (...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs))
 }
+
+/**
+ * 查询是否偏好暗色
+ */
+export const isPreferDark = function () {
+  return window.matchMedia?.('(prefers-color-scheme: dark)')?.matches ?? false
+}
