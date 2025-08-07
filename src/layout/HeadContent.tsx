5a8592f31fd203768f8beb9abfedcf48ea82bc08
someoneonsmile <someoneonsmile@gmail.com>
2025-08-07 14:36:48 +0800

    feat: 修改布局组件

--- src/layout/HeadContent.tsx
+++ src/layout/HeadContent.tsx
@@ -1,14 +1,40 @@
+import { ClassValue } from 'clsx'
 import { ReactNode } from 'react'
+
+import { cm } from '@/util/style'
 
 interface HeadContentProps {
   head: ReactNode
   children?: ReactNode
+  className?: ClassValue
+  headClassName?: ClassValue
+  contentClassName?: ClassValue
+  reverse?: boolean
 }
-export default function HeadContent({ head, children }: HeadContentProps) {
+export default function HeadContent({
+  head,
+  children,
+  className,
+  headClassName,
+  contentClassName,
+  reverse,
+}: HeadContentProps) {
   return (
-    <div className='flex h-full w-full flex-auto flex-col p-2'>
-      <div className='min-h-12'>{head}</div>
-      <div className='min-h-0 flex-auto overflow-y-auto'>{children}</div>
+    <div
+      className={cm(
+        'flex',
+        { 'flex-col-reverse': reverse },
+        'flex-col',
+        'h-full',
+        'w-full',
+        className,
+        className,
+      )}
+    >
+      <div className={cm('flex-none', 'overflow-hidden', headClassName)}>
+        {head}
+      </div>
+      <div className={cm('flex-1', contentClassName)}>{children}</div>
     </div>
   )
 }
--- src/layout/SideContent.tsx
+++ src/layout/SideContent.tsx
@@ -1,15 +1,39 @@
+import { ClassValue } from 'clsx'
 import { ReactNode } from 'react'
+
+import { cm } from '@/util/style'
 
 interface SideContentProps {
   side?: ReactNode
   children?: ReactNode
+  className?: ClassValue
+  sideClassName?: ClassValue
+  contentCalssName?: ClassValue
+  reverse?: boolean
 }
 
-function SideContent({ side, children }: SideContentProps) {
+function SideContent({
+  side,
+  children,
+  className,
+  sideClassName,
+  contentCalssName,
+  reverse,
+}: SideContentProps) {
   return (
-    <div className='flex h-full w-full flex-auto'>
-      <div className='w-5'>{side}</div>
-      <div className='min-h-0 flex-auto overflow-y-auto'>{children}</div>
+    <div
+      className={cm(
+        'flex',
+        { 'flex-row-reverse': reverse },
+        'h-full',
+        'w-full',
+        className,
+      )}
+    >
+      <div className={cm('flex-none', 'overflow-hidden', sideClassName)}>
+        {side}
+      </div>
+      <div className={cm('flex-1', contentCalssName)}>{children}</div>
     </div>
   )
 }
