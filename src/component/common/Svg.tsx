f865fa532d3beead087b93f5f07a1b98887d8a92
someoneonsmile <someoneonsmile@gmail.com>
2025-08-12 08:52:20 +0800

    feat: 添加 Lens svg

--- src/component/common/Svg.tsx
+++ src/component/common/Svg.tsx
@@ -1,5 +1,7 @@
-import EmptyRecordSvg from '@/assets/empty-record.svg?react'
-import EmptyStoreSvg from '@/assets/empty-store.svg?react'
+import EmptyRecordSvg from '@assets/empty-record.svg?react'
+import EmptyStoreSvg from '@assets/empty-store.svg?react'
+import LensSvg from '@assets/lens.svg?react'
 
 export const EmptyStore = EmptyStoreSvg
 export const EmptyRecord = EmptyRecordSvg
+export const Lens = LensSvg
